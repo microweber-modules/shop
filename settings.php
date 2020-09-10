1c6c03046770fac2ec67569fec6771eb58cc5d88
Petko Yovchevski <petko@microweber.com>
2020-09-10 16:19:29 +0300

    updates

--- settings.php
+++ settings.php
@@ -54,15 +54,13 @@ if (isset($_GET['group']) and $_GET['group']) {
 
 <div class="card bg-none style-1 mb-0">
     <div class="card-header">
-        <h5><i class="mdi mdi-cog text-primary mr-3"></i> <strong><?php _e("Shop settings"); ?></strong></h5>
+        <h5><i class="mdi mdi-shopping text-primary mr-3"></i> <strong><?php _e("Shop settings"); ?></strong></h5>
         <div>
 
         </div>
     </div>
 
     <div class="card-body pt-3">
-        <h2 class="text-center mt-3 mb-4">Shop settings</h2>
-        
         <div class="card style-1 mb-3">
             <div class="card-body pt-3 px-5">
                 <div class="row select-settings">
