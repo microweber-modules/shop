60d4ea5cd902d6d0817ecede59176f7f6ece41de
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-03-26 14:36:26 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 $config = array();
 $config['name'] = "Online shop";
 $config['author'] = "Microweber";
-$config['ui_admin'] = false;
-$config['ui'] = false;
+$config['ui_admin'] = true;
+$config['ui'] = true;
 $config['categories'] = "online shop";
 $config['position'] = 2;
-$config['version'] = 0.4;
+$config['version'] = 0.5;
 
 $config['permissions'] = [
     'index' => 'module.admin.shop.index',
